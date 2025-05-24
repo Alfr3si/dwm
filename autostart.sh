bd0508370c83709b671d0097a32eb8698c91774e
Alfr3si <luisalfredolopez510@gmail.com>
2025-05-23 20:47:05 -0600

    add configs

--- autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-feh --bg-scale ~/Wallpapers/astronaut2.png 
-dwmblocks &
-nm-applet &
-picom &
-dunst &
-
--- config.h
+++ config.h
@@ -18,12 +18,13 @@ static const char col_gray2[]       = "#20242c";
 static const char col_gray4[]       = "#4c566a";
 static const char col_marillo[]	    = "#FFF4B7";
 static const char col_grisillo[]    = "#C5D3E8";
-static const char col_green[]       = "#739900";
+static const char col_green[]       = "#a3f59f";
+static const char col_purple[]       = "#7b7f8a";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
-	[SchemeSel]  = { col_white, col_grisillo,  col_grisillo },
+	[SchemeSel]  = { col_white, col_purple,  col_purple },
 };
 /* tagging */
 
@@ -64,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
-
+static const char *keymap[] = { "/home/alfr3d/.local/share/dwm/dwm/script/layout", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
   	{ 0, XK_F1, spawn, SHCMD("pamixer -t && pkill -RTMIN+10 dwmblocks") },
@@ -74,6 +75,7 @@ static const Key keys[] = {
 	{ 0, XK_F6, spawn, SHCMD("brightnessctl set +5% && sleep 0.1 && notify-send -u low ' Brillo' \"$(brightnessctl g | awk '{print int($1 / 21333 * 100)}')%\"") },
   	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { ControlMask|Mod1Mask,         XK_space,  spawn,          {.v = keymap } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
